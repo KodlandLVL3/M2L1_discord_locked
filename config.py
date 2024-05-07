d6eb496cf4d708d47234726d51fcc0cd7a417fad
Kootyra <110038102+Kootyra@users.noreply.github.com>
2024-05-07 16:00:21 +0200

    first commit

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-Python/Lvl3_global/M1L4/M1L4_discord/config.py Python/Lvl3_global/M1L4/M1L4_discord/logic.py Python/Lvl3_global/M1L4/M1L4_discord/main.py
+token = "Здесь должен быть токен твоего бота"
